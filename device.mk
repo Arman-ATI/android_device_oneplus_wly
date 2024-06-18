3d7c596a578b1dbee84c0aeb1740fbf071f42a6c
Arman-ATI <altafiarman5@gmail.com>
2024-06-18 16:05:01 +0330

    wly: Set default hotspot ssid

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
+    OPlus10PROWifiOverlay \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
