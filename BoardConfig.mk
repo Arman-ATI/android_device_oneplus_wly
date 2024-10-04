6e7c47301e7d93fbb9cea51507a94bd09fcec76f
Machad3x <machad3x@gmail.com>
2024-10-03 22:53:28 -0400

    wly: Initial prebuilts from NE2211_14.0.0.920(EX01)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,10 +7,18 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8450-common/BoardConfigCommon.mk
 
+BUILD_BROKEN_SRC_DIR_IS_WRITABLE := true
+
 DEVICE_PATH := device/oneplus/wly
 
 # Display
 TARGET_SCREEN_DENSITY := 450
+
+# DTB/O
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+TARGET_KERNEL_CONFIG += vendor/oplus/wly.config
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
