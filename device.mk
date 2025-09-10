f8cb17436a4c6217c2e199d03dece6c472e58427
Arman-ATI <altafiarman5@gmail.com>
2025-09-10 10:10:18 +0000

    wly: Update from NE2213_15.0.0.901(EX01)

--- device.mk
+++ device.mk
@@ -28,8 +28,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2376
+TARGET_SCREEN_HEIGHT := 2412
 TARGET_SCREEN_WIDTH := 1080
+
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # Display
 PRODUCT_COPY_FILES += \
