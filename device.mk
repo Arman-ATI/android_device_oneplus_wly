47696f40e87d09f5dbba9bda213ac44b2a28df02
pjgowtham <pjgowtham@gmail.com>
2025-10-03 22:34:23 +0530

    wly: Decommonize Lineage Health control charging path
    Change-Id: I5e802bc87a1b2966f8e13190e2df44ac1df63597

--- device.mk
+++ device.mk
@@ -35,9 +35,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946358012694402.xml
 
-# Lineage Health
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
